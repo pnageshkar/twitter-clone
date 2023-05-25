9551885200ff72aa70b766f05a7b2cca82700724
Prasad Nageshkar <prasad.nageshkar@gmail.com>
2023-05-25 18:40:21 +0530

    The Login and Register functionality without auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import Layout from '@/components/Layout';
 import Modal from '@/components/Modal';
+import LoginModal from '@/components/models/LoginModal';
+import RegisterModal from '@/components/models/RegisterModal';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <LoginModal />
+      <RegisterModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
