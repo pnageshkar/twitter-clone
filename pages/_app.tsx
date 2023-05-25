56df97ac53564302457ab5e42095be164c8ac053
Prasad Nageshkar <prasad.nageshkar@gmail.com>
2023-05-25 15:38:36 +0530

    Added Button, Input and Modal components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import Layout from '@/components/Layout';
+import Modal from '@/components/Modal';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
