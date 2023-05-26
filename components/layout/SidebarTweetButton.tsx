f04e191409c5097437f7e702c429fb193fc0ff34
Prasad Nageshkar <prasad.nageshkar@gmail.com>
2023-05-26 17:06:09 +0530

    Setup Prisma env and schema

--- components/layout/SidebarTweetButton.tsx
+++ components/layout/SidebarTweetButton.tsx
@@ -2,10 +2,19 @@ import { useCallback } from 'react';
 import { FaFeather } from 'react-icons/fa';
 import { useRouter } from 'next/router';
 
+import useLoginModal from '@/hooks/useLoginModal';
+
 const SidebarTweetButton = () => {
   const router = useRouter();
+  const loginModal = useLoginModal();
+
+  const onClick = useCallback(() => {
+    loginModal.onOpen();
+    //router.push('/');
+  }, [loginModal]);
+
   return (
-    <div onClick={() => router.push('/')}>
+    <div onClick={onClick}>
       <div
         className='
       mt-6
