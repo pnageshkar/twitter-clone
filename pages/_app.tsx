5b73a30c726a4024f67e3da9332d207a5e3a47fb
Prasad Nageshkar <prasad.nageshkar@gmail.com>
2023-05-29 22:48:11 +0530

    User Functionalty completed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,13 @@ import Layout from '@/components/Layout';
 import LoginModal from '@/components/modals/LoginModal';
 import RegisterModal from '@/components/modals/RegisterModal';
 import '@/styles/globals.css';
+import EditModal from '@/components/modals/EditModal';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Toaster />
+      <EditModal />
       <LoginModal />
       <RegisterModal />
       <Layout>
